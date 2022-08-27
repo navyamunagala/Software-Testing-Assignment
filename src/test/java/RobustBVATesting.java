49a9e7430ba4309d17abc701c35d07a6b3fe8b87
Navya Munagala <navyamungala25@gmail.com>
2022-08-27 14:16:54 +0530

    testing robust BVA

--- src/test/java/RobustBVATesting.java
+++ src/test/java/RobustBVATesting.java
@@ -10,16 +10,16 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class TestBookHouseWebPageExistingUser {
+public class RobustBVATesting {
     public static final String EMAIL_ID = "2021ct93022@wilp.bits-pilani.ac.in";
     public static final String PASSWORD = "Hello12#";
     public static final String BASE_WEB_URL = "https://www.sapnaonline.com/";
-    public static final String FIREFOX_DRIVER_ABSOLUTE_PATH = "C:\\webdrivers\\chromedriver.exe";
+    public static final String CHROME_DRIVER_ABSOLUTE_PATH = "C:\\webdrivers\\chromedriver.exe";
     WebDriver driver;
 
     @BeforeTest
     void setUp() {
-        System.setProperty("webdriver.chrome.driver", FIREFOX_DRIVER_ABSOLUTE_PATH);
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_ABSOLUTE_PATH);
         driver = new ChromeDriver();
     }
 
@@ -46,33 +46,33 @@ public class TestBookHouseWebPageExistingUser {
         WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Please enter a valid email')]"));
         assertEquals("Please enter a valid email",element.getText());
     }
-
+// less than 6
     @Test
-    void exit_Invalid_lenpwdTest() {
+    void pwdlenlessthansix_Test1() {
         driver.get(BASE_WEB_URL);
         driver.findElement(By.cssSelector("div[class = 'sc-Axmtr cTkVAB']")).click();
         driver.findElement(By.cssSelector("a[class = 'item']")).click();
         driver.findElement(By.cssSelector("input[type = email]")).sendKeys(EMAIL_ID);
-        driver.findElement(By.cssSelector("input[type = password]")).sendKeys("snfl");
+        driver.findElement(By.cssSelector("input[type = password]")).sendKeys("snflu");
         driver.findElement(By.cssSelector("div[class = 'sc-Axmtr iPDAko ButtonText']")).click();
         WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Password length should be between 6 and 20 characters')]"));
         assertEquals("Password length should be between 6 and 20 characters",element.getText());
 
     }
 
-//    @Test
-//    void exit_Incorrect_pwdTest() {
-//
-//        driver.get(BASE_WEB_URL);
-//        driver.findElement(By.cssSelector("div[class = 'sc-Axmtr cTkVAB']")).click();
-//        WebElement emailElement = driver.findElement(By.id("email"));
-//        emailElement.sendKeys(" ");
-//        WebElement passwordElement = driver.findElement(By.id("pass"));
-//        passwordElement.sendKeys("your password here");
-//        driver.findElement(By.cssSelector("div[class = 'sc-Axmtr iPDAko ButtonText']")).click();
-//
-//    }
+// pwd length more than 20
 
+    @Test
+    void Invalid_lenpwd_twenty_Test2() {
+        driver.get(BASE_WEB_URL);
+        driver.findElement(By.cssSelector("div[class = 'sc-Axmtr cTkVAB']")).click();
+        driver.findElement(By.cssSelector("a[class = 'item']")).click();
+        driver.findElement(By.cssSelector("input[type = email]")).sendKeys(EMAIL_ID);
+        driver.findElement(By.cssSelector("input[type = password]")).sendKeys("dfngkdlgnkdfgnngjyhjj");
+        driver.findElement(By.cssSelector("div[class = 'sc-Axmtr iPDAko ButtonText']")).click();
+        WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Password length should be between 6 and 20 characters')]"));
+        assertEquals("Password length should be between 6 and 20 characters",element.getText());
+    }
 
 
 
